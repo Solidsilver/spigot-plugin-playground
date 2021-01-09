644ae6f1e930bbea30a263a712e2b9e8f8db852f
Luke Mattfeld <lukemattfeld@gmail.com>
2021-01-09 13:34:56 -0800

    Updated pack data, link for auto-download to fix cert error

--- src/dev/solidsilver/testplugin/JoinEvent.java
+++ src/dev/solidsilver/testplugin/JoinEvent.java
@@ -8,7 +8,7 @@ public class JoinEvent implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-//        String packUrl = "https://serve.solidsilver.dev/CustomItemsTest_latest.zip";
-//        event.getPlayer().setResourcePack(packUrl);
+       String packUrl = "https://download.mc-packs.net/pack/e30356494ccfb1ef567ca0e60b8f46935ab99d9b.zip";
+       event.getPlayer().setResourcePack(packUrl);
     }
 }
